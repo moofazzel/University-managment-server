caeb8ac0906d0eaa77a8733cabc0ec4e055b2534
moofazzel <2192461016@uttarauniversity.edu.bd>
2023-11-06 19:02:14 +0600

    add a test for file for my git alises for bash

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -15,21 +15,3 @@ export const generateUserId = async () => {
 
   return incrementedId
 }
-
-// import { User } from './user.model'
-
-// const findLastUserId = async () => {
-//   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
-//     .sort({
-//       createdAt: -1,
-//     })
-//     .lean()
-//   return lastUser?.id
-// }
-
-// export const generateUserId = async () => {
-//   const currentId = (await findLastUserId()) || '00000'
-//   const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
-
-//   return incrementedId
-// }
